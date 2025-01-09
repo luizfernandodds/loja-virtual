f893318311c55c4accabc17c1cac20d01c5a3757
luizf <luizf@Nando>
2025-01-08 21:58:40 -0400

    Created entity ProductImage

---
+++ src/main/java/com/dev/loja_virtual/model/ProductImage.java
@@ -0,0 +1,48 @@
+package com.dev.loja_virtual.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "product_image")
+@SequenceGenerator(name = "seq_product_image", sequenceName = "seq_product_image", initialValue = 1, allocationSize = 1)
+public class ProductImage implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_product_image")
+	private Long id;
+	
+	@Column(name = "image_original", columnDefinition = "text")
+	private String imageOriginal;
+	
+	@Column(name = "image_thumbnail", columnDefinition = "text")
+	private String imageThumbnail;
+	
+	@ManyToOne
+	@JoinColumn(name = "product_id", nullable = false,
+	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "product_fk"))
+	private Product product;
+	
+}
